e7174e622883d7f45e3a76399d6a2ca2aaf38802
Benetti-hub <farmforfun96@gmail.com>
2021-09-05 16:06:27 -0400

    Added multithreading + mock links file

---
+++ src/scraper.py
@@ -0,0 +1,34 @@
+'''Core script for scrape Linkedin Job pages'''
+import fileinput
+import multiprocessing
+from webscraper import WebScraper
+
+def collect_jobs(url):
+    '''
+    Collect information about the jobs in the
+    page. For more info check the WebScraper
+    class.
+    '''
+    scraper = WebScraper()
+    scraper.scrape_linkedin(url)
+
+    print("Collected infos!")
+
+def get_urls():
+    '''
+    Read the urls in the links.txt file and
+    parse it as a list
+    '''
+    with fileinput.input(files=('links.txt')) as f:
+        urls = []
+        for line in f:
+            urls.append(line)
+
+    return urls
+
+if __name__ == "__main__":
+
+    urls = get_urls()
+    #Go parallel!
+    pool_obj = multiprocessing.Pool()
+    pool_obj.map(collect_jobs, urls)
--- src/webscraper.py
+++ src/webscraper.py
@@ -8,66 +8,137 @@ import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+SLEEP_TIME = 2
+
 class WebScraper():
     '''
-    Base class to perfrom web-scraping on linkedin
+    Simple class to perform Web-Scraping on Linkedin.
+    It takes as input an url and collect information
+    about the jobs in the loaded webpage. It
     '''
     def __init__(self):
         self.OS = platform.system()
+        self.wd = ""
         self.verbose = 1
 
-    def remove_cookies(self, wd):
-        wd.find_element_by_xpath('/html/body/div[1]/div/section/div/div[2]/button[2]').click()
+    def remove_cookies(self):
+        '''
+        Remove the cookies from the webpage.
+        This is actually not required, but I hate
+        cookies.
+        '''
+
+        self.wd.find_element_by_xpath('/html/body/div[1]'
+                                      '/div/section/div/div[2]/button[2]').click()
 
     def get_wd(self):
-
         '''
-        Load the correct webdriver given the OS
+        Enable to load the correct webdriver given
+        the operating system. Currently there is no
+        option for Mac Systems, since I have none at
+        home >:(
+
+        Input:
+            None
+
+        Return:
+            The correct webdriver
         '''
         chrome_options = Options()
         chrome_options.add_argument("--window-size=1920,1080")
         if self.OS == "Linux":
-            wd = webdriver.Chrome('./driver/Linux_chromedriver', options=chrome_options)
+            self.wd = webdriver.Chrome('./driver/Linux_chromedriver',
+                                       options=chrome_options)
         elif self.OS == "Windows":
-            wd = webdriver.Chrome('./driver/Windows_chromedriver.exe', options=chrome_options)
+            self.wd = webdriver.Chrome('./driver/Windows_chromedriver.exe',
+                                       options=chrome_options)
 
-        return wd
+    def load_jobs(self):
+        '''
+        Scrool for the full length of the page.
+        Each loop check if the "Show more" button
+        is enabled, and try to click it if possible.
 
-    def connect_to_linkedin(self, url):
+        Input:
+            wd : the webdriver
+
+        returns:
+            the full web page with all jobs loaded
+        '''
+
+        s = self.wd.find_element_by_css_selector('h1>span').get_attribute('innerText')
+        jobs = int(re.sub(r'[^\w]', '', s))
+
+        for _ in range(int(jobs/25)):
+            self.wd.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            time.sleep(SLEEP_TIME)
+            try:
+                self.wd.find_element_by_xpath('//*[@id="main-content"]/section[2]/button').click()
+            except:
+                pass
+
+        #Returns at top of the page
+        self.wd.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+
+        job_lists = self.wd.find_element_by_class_name('jobs-search__results-list')
+        return job_lists.find_elements_by_tag_name('li')
+
+    def collect_infos(self, job):
+        '''
+        Utility function to collect all the relevant info
+        about a given job.
+
+        Input:
+            job : the job to analyze
+            rec : variable to manage network lag spikes
+
+        Returns:
+            job_info : a dictionary containing all the
+                       info about a job
+        '''
+        job_info = {
+            'id' : job.id,
+            'title' : job.find_element_by_css_selector('h3').get_attribute('innerText'),
+            'company_name' : job.find_element_by_css_selector('h4').get_attribute('innerText'),
+            'location' : job.find_element_by_css_selector('[class="job-search'+
+                                '-card__location"]').get_attribute('innerText'),
+            'date_posted' : job.find_element_by_css_selector('div>div>time').get_attribute('datetime'),
+            'link' : job.find_element_by_css_selector('a').get_attribute('href'),
+            'description' : job.find_element_by_xpath('/html/body/div[1]/div/section'+
+                            '/div[2]/section[2]').get_attribute('innerText')
+        }
+
+        return job_info
+
+
+    def scrape_linkedin(self, url):
 
         if self.verbose:
             print("Connecting to Linkedin")
 
-        wd = self.get_wd()
-        wd.get(url)
+        self.get_wd()
+        self.wd.get(url)
 
-        time.sleep(3)
-        self.remove_cookies(wd)
+        time.sleep(SLEEP_TIME)
+        self.remove_cookies()
+        jobs = self.load_jobs()
 
-        s = wd.find_element_by_css_selector('h1>span').get_attribute('innerText')
-        jobs = int(re.sub(r'[^\w]', '', s))
-        i = 2
-        while i <= int(jobs/25)+1:
-            wd.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-            i = i + 1
-            time.sleep(3)
-            try:
-                wd.find_element_by_xpath('//*[@id="main-content"]/section[2]/button').click()
-            except:
-                pass
+        if self.verbose:
+            print("Collecting infos!")
 
+        job_info = []
+        for job in jobs:
+            job.click()
+            job_info.append(self.collect_infos(job))
+            time.sleep(SLEEP_TIME)
 
+        df = pd.DataFrame(job_info)
 
 
 
 
 
 
-if __name__ == "__main__":
-
-    url = "https://www.linkedin.com/jobs/search/?geoId=106693272&keywords=machine%20learning%20engineer&location=Switzerland"
-    scraper = WebScraper()
-    scraper.connect_to_linkedin(url)
 
 
 
